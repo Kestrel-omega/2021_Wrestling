5cd1b5a675c8a9bfdade9f6166f28eb1abd94f48
LHE9898 <92521680+LHE9898@users.noreply.github.com>
2021-10-21 22:22:18 +0900

    Update Wrestling_Header.h

--- Wrestling_Header.h
+++ Wrestling_Header.h
@@ -1,76 +1,125 @@
-#pragma once
-#ifndef __WRESTLING_HEADER_H__
-#define __WRESTLING_HEADER_H__
-
-#include <Arduino.h>
-#include <SoftwareSerial.h>
-#include <Wire.h>
-#include <String.h>
-#include <stdio.h>
-#include "Adafruit_TCS34725softi2c.h"
-
-// Pinmap setting for arduino mega 2560
-#define IR1 A0
-#define IR2 A2
-#define IR3 A4
-#define IR4 A6
-#define SDApin1 2
-#define SDApin2 3
-#define SDApin3 4
-#define SDApin4 5
-#define L_EN1 13
-#define R_EN1 12
-#define L_PWM1 11 //back
-#define R_PWM1 10 //forward
-#define L_EN2 9
-#define R_EN2 8
-#define L_PWM2 7  // forward
-#define R_PWM2 6 //back
-#define TX1 18
-#define RX1 19
-#define SCLpin 20
-
-// Macro of motor
-#define ON true
-#define OFF false
-#define R1 101
-#define R2 102
-#define L1 111
-#define L2 112
-
-// Declare color class
-struct Color
-{
-    float red;
-    float green;
-    float blue;
-};
-
-// Initialize
-void InitIRSensor();        // Initialize IR sensors
-void InitColorSensor();     // Initialize color sensors
-void InitMotor();    // Initialize motor settings
-void InitCamSerial(struct SoftwareSerial mySerial, int baudrate);       // Initialize jetson - arduino serial communication settings
-void InitBot(struct SoftwareSerial mySerial, int baudrate);             // Initialize all of things
+#include "Wrestling_Header.h"
 
 // IR sensor library
-void IR_safe(); // IR 2,3,4 far from 25cm
-float ReadIRSensor(int idx);    // Read IR sensor to get a distance from obstacles (idx : 0~3)
+float ReadIRSensor(int idx) // Read IR sensor to get a distance from obstacles
+{
+	float sensorValue = 0;
+
+	switch (idx)
+	{
+		case 0:
+		sensorValue = analogRead(IR1);
+		break;
+
+		case 1:
+		sensorValue = analogRead(IR2);
+		break;
+
+		case 2:
+		sensorValue = analogRead(IR3);
+		break;
+
+		case 3:
+		sensorValue = analogRead(IR4);
+		break;
+	}
+	float cm = 10650.08 * pow(sensorValue, -0.935) - 10; // need to check
+
+	return cm;
+}
+
+bool IR_safe()
+{
+	return ReadIRSensor(1) >= 25  && ReadIRSensor(2) >= 25  && ReadIRSensor(3) >= 25;
+}
 
 // Color sensor library
-void Color_black(); //color0,1,2,3 all black
-void Color_red_front(); //color 0 or 1 is red
-void Color_red_back(); // color 2 or 3 is red
-void Color_left();
-void Color_right();
-struct Color ReadColorSensor(struct Adafruit_TCS34725softi2c tcs); // Read color sensor in r, g, b value
+struct Color ReadColorSensor(struct Adafruit_TCS34725softi2c tcs) // Read color sensor in r, g, b value
+{
+	uint16_t clear, red, green, blue;
+	uint32_t sum = clear;
+	float r, g ,b;
+
+
+	Color color;
+	tcs.setInterrupt(false);
+	delay(60);
+
+	tcs.getRawData(&red, &green, &blue, &clear);
+	tcs.setInterrupt(true);
+	
+	r = red ; r /= 256;
+	g = green; g /=256;
+	b = blue; b/= 256;
+	
+	
+
+	color.red = red;
+	color.green = green;
+	color.blue = blue;
+	
+	return color;
+}
 
 // Motor control library
-void StopMotor(); // shut down all motor
-void MotorActiveStatus(int a); // Set motor turn on/off, a==1: turn on, a==0 : turn off
+void InitMotor()
+{
+	for(int i=6;i<14;i++)
+	{
+		pinMode(i,OUTPUT);
+		digitalWrite(i,LOW);
+	}
+}
 
-// Camera serial library
-void Cam_found();
-void ReadCamData(struct SoftwareSerial mySerial, int &x_value, int &y_value, int &size_value);          // Read camera data from jetson nano
+void StopMotor() // shut down all motor
+{
+	digitalWrite(R_EN1,LOW);
+	digitalWrite(L_EN1,LOW);
+	digitalWrite(R_EN2,LOW);
+	digitalWrite(L_EN2,LOW);
+	analogWrite(R_PWM1,0);
+	analogWrite(L_PWM1,0);
+	analogWrite(R_PWM2,0);
+	analogWrite(L_PWM2,0);
+}
 
-#endif
+void MotorActiveStatus(int a) // Set motor turn on/off
+{
+	if(a == 1)
+	{
+		digitalWrite(R_EN1,HIGH);
+		digitalWrite(L_EN1,HIGH);
+		digitalWrite(R_EN2,HIGH);
+		digitalWrite(L_EN2,HIGH);
+	}
+	
+	else if(a==0)
+	{
+		digitalWrite(R_EN1,LOW);
+		digitalWrite(L_EN1,LOW);
+		digitalWrite(R_EN2,LOW);
+		digitalWrite(L_EN2,LOW);
+	}
+
+}
+
+// Initialize
+void InitIRSensor() // Initialize IR sensors
+{
+	pinMode(IR1, INPUT);
+	pinMode(IR2, INPUT);
+	pinMode(IR3, INPUT);
+	pinMode(IR4, INPUT);
+}
+
+void InitCamSerial(struct SoftwareSerial mySerial, int baudrate) // Initialize jetson - arduino serial communication settings
+{
+	Serial.begin(baudrate);
+	mySerial.begin(baudrate);
+}
+void InitBot(struct SoftwareSerial mySerial, int baudrate) // Initialize all of things
+{
+	InitIRSensor();
+	InitColorSensor();
+	InitCamSerial(mySerial, baudrate);
+}
